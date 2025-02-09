2be70c36b843c9e0ec1415a7d8ca938d801e98d2
Daniel C <daniel.cao@student.tdsb.on.ca>
2025-02-09 16:37:39 -0500

    first commit

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    
+    public static void main(String[] args) {
+        System.out.println("test");
+        System.out.println(AddTwo(2, 3));
+    }
+
+    public static int AddTwo(int num1, int num2) {
+        return num1 + num2;
+    }
+
+}
---
+++ test.py
@@ -0,0 +1,8 @@
+
+# cool comment
+
+
+def AddTwo(num1, num2):
+    return num1 + num2
+
+print(AddTwo(1, 4))
