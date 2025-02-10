e1a8a586080f67ebb0d4929bc4e20e55228af9f8
Daniel C <bingrongcao@gmail.com>
2025-02-09 19:07:19 -0500

    first branch commit

--- Main.java
+++ Main.java
@@ -2,6 +2,7 @@ public class Main {
     
     public static void main(String[] args) {
         System.out.println("test");
+        System.out.println("testing branches");
         System.out.println(AddTwo(2, 3));
     }
 
--- test.py
+++ test.py
@@ -5,4 +5,6 @@
 def AddTwo(num1, num2):
     return num1 + num2
 
+print("apple")
+
 print(AddTwo(1, 4))
